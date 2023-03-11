02d5822a958c75642b79cd11cf4bf6fbc22da8ad
pottor02 <doramon_toon_5@hotmail.com>
2023-03-11 18:22:28 +0700

    Add line part 01

--- Challenge02.js
+++ Challenge02.js
@@ -6,3 +6,4 @@ let card3 = 'I like Snack and Car.'
 // Display the card with the information in the correct place.
 console.log(card1+card2+card3)
 // Example: “Hi, my name is John. I live in South Africa and enjoy drinking homemade wine.”
+
