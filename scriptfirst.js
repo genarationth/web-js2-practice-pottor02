1032bd9bc800877d24bc433f994cc2fa3d88acb6
pottor02 <doramon_toon_5@hotmail.com>
2023-03-11 18:11:09 +0700

    FinishPart01

--- scriptfirst.js
+++ scriptfirst.js
@@ -53,3 +53,54 @@ console.log("NaN")
 console.log("Number : " + Number(h))
 console.log("String : " + String(h))
 console.log("Boolean: " + Boolean(h))
+
+
+// Infinity
+let i = Infinity;
+console.log("Infinity")
+console.log("Number : " + Number(i))
+console.log("String : " + String(i))
+console.log("Boolean: " + Boolean(i))
+
+// -Infinity
+let j = -Infinity;
+console.log("-Infinity")
+console.log("Number : " + Number(j))
+console.log("String : " + String(j))
+console.log("Boolean: " + Boolean(j))
+
+// ""
+let k = "";
+console.log("DoubleQuote")
+console.log("Number : " + Number(k))
+console.log("String : " + String(k))
+console.log("Boolean: " + Boolean(k))
+
+// "20"
+let l = "20";
+console.log("20")
+console.log("Number : " + Number(l))
+console.log("String : " + String(l))
+console.log("Boolean: " + Boolean(l))
+
+// "Twenty"
+let m = "Twenty";
+console.log("'Twenty'")
+console.log("Number : " + Number(m))
+console.log("String : " + String(m))
+console.log("Boolean: " + Boolean(m))
+
+// null
+let n = null;
+console.log("null")
+console.log("Number : " + Number(n))
+console.log("String : " + String(n))
+console.log("Boolean: " + Boolean(n))
+
+
+// undefinited
+let o = undefinited;
+console.log("undefinited")
+console.log("Number : " + Number(o))
+console.log("String : " + String(o))
+console.log("Boolean: " + Boolean(o))
