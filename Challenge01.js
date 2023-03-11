43fa60280a1844563cae3461eda21b2220269e92
pottor02 <doramon_toon_5@hotmail.com>
2023-03-11 18:17:51 +0700

    Final All

---
+++ Challenge01.js
@@ -0,0 +1,6 @@
+// Store your birth year in a variable.
+let myBirthYear = 1990
+// Store a future year in another variable.
+let futureYear = 2089
+// Calculate your possible ages for the future year and display it in the console.
+console.log(futureYear-myBirthYear)
---
+++ Challenge02.js
@@ -0,0 +1,8 @@
+// Create a card holding 3 pieces of information about a person, such as Name, Location, and Hobby.
+// Each piece of information should be a variable.
+let card1 = 'Hello My name is KaTooN.'
+let card2 = 'I live in Bangkok, Thailand.'
+let card3 = 'I like snack.'
+// Display the card with the information in the correct place.<br>
+console.log(card1+card2+card3)
+// Example: “Hi, my name is John. I live in South Africa and enjoy drinking homemade wine.”
