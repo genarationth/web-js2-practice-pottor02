0ff59e4342618ded7cdd208da2ba025c18a5d622
pottor02 <doramon_toon_5@hotmail.com>
2023-03-11 18:22:40 +0700

    Part01

--- Challenge01.js
+++ Challenge01.js
@@ -2,5 +2,10 @@
 let myBirthYear = 1990
 // Store a future year in another variable.
 let futureYear = 2089
+
+currentAge = futureYear-myBirthYear
+possibleAge = currentAge - 1
 // Calculate your possible ages for the future year and display it in the console.
 console.log(futureYear-myBirthYear)
+
+console.log(`If you were born in ${myBirthYear}, then in ${futureYear} you'll be ${possibleAge} or ${currentAge} (depending on the month in ${futureYear}).`);
