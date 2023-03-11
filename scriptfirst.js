a94318f01d6a6f2ebae5d5d2ec24f5ae1493ddc9
pottor02 <doramon_toon_5@hotmail.com>
2023-03-11 18:00:38 +0700

    FirstTime

---
+++ scriptfirst.js
@@ -0,0 +1,55 @@
+// false
+let a = false;
+console.log("false")
+console.log("Number : " + Number(a))
+console.log("String : " + String(a))
+console.log("Boolean: " + Boolean(a))
+
+// true
+let b = true;
+console.log("true")
+console.log("Number : " + Number(b))
+console.log("String : " + String(b))
+console.log("Boolean: " + Boolean(b))
+
+// 0
+let c = 0;
+console.log("0")
+console.log("Number : " + Number(c))
+console.log("String : " + String(c))
+console.log("Boolean: " + Boolean(c))
+
+// 1
+let d = 1;
+console.log("1")
+console.log("Number : " + Number(d))
+console.log("String : " + String(d))
+console.log("Boolean: " + Boolean(d))
+
+// "0"
+let e = "0";
+console.log("'0'")
+console.log("Number : " + Number(e))
+console.log("String : " + String(e))
+console.log("Boolean: " + Boolean(e))
+
+// "000"
+let f = "000";
+console.log("'000'")
+console.log("Number : " + Number(f))
+console.log("String : " + String(f))
+console.log("Boolean: " + Boolean(f))
+
+// "1"
+let g = "1";
+console.log("'1'")
+console.log("Number : " + Number(g))
+console.log("String : " + String(g))
+console.log("Boolean: " + Boolean(g))
+
+// NaN
+let h = NaN;
+console.log("NaN")
+console.log("Number : " + Number(h))
+console.log("String : " + String(h))
+console.log("Boolean: " + Boolean(h))
